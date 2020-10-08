7abf606d64f05ba8943fba55a9372c8709bcb3f3
Victor Miguez <victorcineis@gmail.com>
2020-10-08 22:53:58 +1100

    Atualiza lista de candidatos de Campinas

--- src/fetcher.js
+++ src/fetcher.js
@@ -2,37 +2,42 @@ var fs = require('fs');
 var path = require('path');
 const fetch = require('node-fetch');
 
-var candidates = require('../candidates-data/2020/recife.json');
-
 const dataDir = path.resolve(__dirname, '..', 'candidates-data');
-const candidateUrl =
-  'http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/buscar/2020/25313/2030402020/candidato';
+// const candidateUrl =
+//   'http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/buscar/2020/25313/2030402020/candidato';
 
-const fetchCandidatesDetails = async (data, outputFile) => {
-  for (let i = 0; i < data.candidatos.length; i++) {
-    const candidate = data.candidatos[i];
+const campinas = {
+  candidates: require('../candidates-data/2020/campinas.json'),
+  candidatesUrl: 'http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/listar/2020/62910/2030402020/13/candidatos',
+  candidateDetailsUrl: 'http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/buscar/2020/62910/2030402020/candidato',
+  outputFile: 'campinas-details2.json'
+}
 
-    await fetch(`${candidateUrl}/${candidate.id}`)
-      .then((response) => response.json())
-      .then((details) => {
-        const data = {
-          tseId: candidate.id,
-          number: details.numero,
-          picture: details.fotoUrl,
-        };
+const fetchCandidatesDetails = async (city) => {
+  // for (let i = 0; i < city.candidates.candidatos.length; i++) {
+  const candidate = city.candidates.candidatos.filter(i => i.id === 250000898203)[0];
 
-        fs.appendFile(`${dataDir}/${outputFile}`, JSON.stringify(data), (error) => {
-          if (error) {
-            console.log('Falhou escrita' + candidate.id);
-            console.log(error);
-            return;
-          }
+  await fetch(`${city.candidateDetailsUrl}/${candidate.id}`)
+    .then((response) => response.json())
+    .then((details) => {
+      const data = {
+        tseId: candidate.id,
+        number: details.numero,
+        picture: details.fotoUrl,
+      };
 
-          console.log(i, candidate.id);
-        });
-      })
-      .catch(() => console.error('Falhou busca' + candidate.id));
-  }
+      fs.appendFile(`${dataDir}/${city.outputFile}`, `${JSON.stringify(data)},\n`, (error) => {
+        if (error) {
+          console.log('Falhou escrita' + candidate.id);
+          console.log(error);
+          return;
+        }
+
+        console.log(i, candidate.id);
+      });
+    })
+    .catch(() => console.error('Falhou busca' + candidate.id));
+  // }
 };
 
-fetchCandidatesDetails(candidates, 'recife-details.json');
+fetchCandidatesDetails(campinas);
